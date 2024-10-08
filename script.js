d72e8ee6d1e120384bf96263ad3d51fc6529281c
Jessica <gebhardt.jessica@gmx.net>
2024-10-08 14:28:55 +0200

    fixed section navigation

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function handleIntersection(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const sectionId = entry.target.id;
+            history.replaceState(null, null, `#${sectionId}`);
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersection, {
+    root: null, // Ansichtsfenster
+    threshold: 0.5 // Mindestens 50% der Section muss sichtbar sein
+});
+
+const sections = document.querySelectorAll('section');
+sections.forEach(section => {
+    observer.observe(section);
+});
