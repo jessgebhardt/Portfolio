dad48e8eaea1ff6fdaf5eae3a5f83aefb1bb4210
Jessica <gebhardt.jessica@gmx.net>
2024-10-08 14:53:02 +0200

    startet implementing theme button

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Toggle Menu */
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
@@ -5,6 +7,8 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+/* Section Obvserver */
 
 function handleIntersection(entries) {
     entries.forEach(entry => {
@@ -23,4 +27,29 @@ const observer = new IntersectionObserver(handleIntersection, {
 const sections = document.querySelectorAll('section');
 sections.forEach(section => {
     observer.observe(section);
+});
+
+/* Toggle Theme */
+
+const themeToggleButton = document.getElementById('theme-toggle');
+const bodyElement = document.body;
+
+// Überprüfe, ob ein gespeichertes Theme existiert
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme) {
+    bodyElement.className = savedTheme;
+    themeToggleButton.textContent = savedTheme === 'dark-theme' ? 'Helles Theme aktivieren' : 'Dunkles Theme aktivieren';
+}
+
+themeToggleButton.addEventListener('click', () => {
+    // Theme umschalten
+    if (bodyElement.classList.contains('light-theme')) {
+        bodyElement.classList.replace('light-theme', 'dark-theme');
+        themeToggleButton.textContent = 'Helles Theme aktivieren'; // Button-Text anpassen
+        localStorage.setItem('theme', 'dark-theme'); // Speichere das Theme
+    } else {
+        bodyElement.classList.replace('dark-theme', 'light-theme');
+        themeToggleButton.textContent = 'Dunkles Theme aktivieren'; // Button-Text anpassen
+        localStorage.setItem('theme', 'light-theme'); // Speichere das Theme
+    }
 });
