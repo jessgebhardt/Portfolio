6f1c5d9312c0327a31da4569a25a33d587719fc2
Jessica <gebhardt.jessica@gmx.net>
2024-10-14 14:52:31 +0200

    Added favicon with credits

--- script.js
+++ script.js
@@ -24,3 +24,25 @@ const sections = document.querySelectorAll('section');
 sections.forEach(section => {
     observer.observe(section);
 });
+
+/* Modal */
+
+var modal = document.getElementById("creditsModal");
+
+var modalBtn = document.getElementById("openModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+modalBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
